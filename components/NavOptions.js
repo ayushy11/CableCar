7c29f23301bd72106def96cf655ff4e100fcb44f
AYUSH YADAV <ayushexx@gmail.com>
2021-12-27 17:20:51 +0530

    feat: added react-navigation/native

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -1,3 +1,4 @@
+import { useNavigation } from "@react-navigation/native";
 import React from "react";
 import { Text, View, Image } from "react-native";
 import { Icon } from "react-native-elements";
@@ -20,13 +21,18 @@ const data = [
 ];
 
 const NavOptions = () => {
+  const navigation = useNavigation();
+
   return (
     <FlatList
       data={data}
       horizontal
       keyExtractor={(item) => item.id}
       renderItem={({ item }) => (
-        <TouchableOpacity style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
+        <TouchableOpacity
+          style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}
+          onPress={() => navigation.navigate(item.screen)}
+        >
           <View>
             <Image
               style={{ width: 120, height: 120, resizeMode: "contain" }}
