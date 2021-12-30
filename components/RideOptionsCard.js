2dc84d954813e1f1e2f4641885254bdc83f89a7e
AYUSH YADAV <ayushexx@gmail.com>
2021-12-30 12:18:11 +0530

    feat: rideoptions card

--- components/RideOptionsCard.js
+++ components/RideOptionsCard.js
@@ -1,11 +1,93 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import React, { useState } from "react";
+import { StyleSheet, Text, SafeAreaView, View } from "react-native";
+import { Icon } from "react-native-elements";
+import { FlatList, TouchableOpacity } from "react-native-gesture-handler";
+import { useDispatch } from "react-redux";
+import tw from "tailwind-react-native-classnames";
+import { setDestination } from "../slices/navSlice";
+import NavFavourites from "./NavFavourites";
+
+const data = [
+  {
+    id: "CableCar-X-1",
+    title: "CableCar-X",
+    multiplier: 1,
+    image: "https://links.papareact.com/3pn",
+  },
+  {
+    id: "CableCar-XL-2",
+    title: "CableCar-XL",
+    multiplier: 1.2,
+    image: "https://links.papareact.com/5w8",
+  },
+  {
+    id: "CableCar-LUX-3",
+    title: "CableCar-LUX",
+    multiplier: 1.75,
+    image: "https://links.papareact.com/7pf",
+  },
+];
 
 const RideOptionsCard = () => {
+  const navigation = useNavigation();
+  const [selected, setSelected] = useState(null);
+
   return (
-    <View>
-      <Text>Pick a ride</Text>
-    </View>
+    <SafeAreaView style={tw`bg-white flex-grow`}>
+      <View>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("NavigateCard")}
+          style={tw`absolute top-3 left-5 p-3 rounded-full`}
+        >
+          <Icon
+            name="caret-back-outline"
+            type="ionicon"
+            color="black"
+            size={16}
+          />
+        </TouchableOpacity>
+        <Text style={tw`text-center py-5 text-xl`}>Select a Ride</Text>
+      </View>
+
+      <FlatList
+        data={data}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item: { id, title, multiplier, image }, item }) => (
+          <TouchableOpacity
+            onPress={() => setSelected(item)}
+            style={tw`flex-row items-center justify-between px-10 ${
+              id === selected?.id && "bg-gray-200"
+            }`}
+          >
+            <Image
+              style={{
+                width: 100,
+                height: 100,
+                resizeMode: "contain",
+              }}
+              source={{ uri: image }}
+            />
+            <View style={tw`-ml-6`}>
+              <Text style={tw`text-xl font-semibold`}>{title}</Text>
+              <Text>Travel time...</Text>
+            </View>
+            <Text style={tw`text-xl`}>$99</Text>
+          </TouchableOpacity>
+        )}
+      />
+
+      <View>
+        <TouchableOpacity
+          disabled={!selected}
+          style={tw`bg-black py-3 m-3 ${!selected && "bg-gray-300"}`}
+        >
+          <Text style={tw`text-xl text-center text-white`}>
+            Choose {selected?.title}
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 };
 
