66693d2f0960d58f667f5ea5359917416fafe067
AYUSH YADAV <ayushexx@gmail.com>
2021-12-30 12:54:04 +0530

    feat: traveltime & travelprice - using redux store

--- components/Map.js
+++ components/Map.js
@@ -3,14 +3,21 @@ import { StyleSheet, Text, View } from "react-native";
 import MapView, { Marker } from "react-native-maps";
 import tw from "tailwind-react-native-classnames";
 import { useSelector } from "react-redux";
-import { selectDestination, selectOrigin } from "../slices/navSlice";
+import {
+  selectDestination,
+  selectOrigin,
+  setTravelTimeInformation,
+} from "../slices/navSlice";
 import MapViewDirections from "react-native-maps-directions";
 
 import { MAPBOX_TOKEN } from "@env";
+import { useDispatch } from "react-redux";
 
 const Map = () => {
   const origin = useSelector(selectOrigin);
   const destination = useSelector(selectDestination);
+
+  const dispatch = useDispatch();
 
   // origin.location.lat
   // origin.location.lng
@@ -19,12 +26,23 @@ const Map = () => {
 
   //   useEffect(() => {
   //     if (!origin || !destination) return;
-
   //     mapRef.current.fitToSuppliedMarkers(["origin", "destination"], {
   //       edgePadding: { top: 50, right: 50, left: 50, bottom: 50 },
   //     });
-
   //   }, [origin, destination]);
+
+  useEffect(() => {
+    if (!origin || !destination) return;
+
+    const getTravelTime = async () => {
+      fetch(`https://mapbox`)
+        .then((res) => res.json())
+        .then((data) => {
+          dispatch(setTravelTimeInformation(data.rows[0].elements[0]));
+        });
+    };
+    getTravelTime();
+  }, [origin, destination, MAPBOX_TOKEN]);
 
   return (
     <MapView
@@ -35,6 +53,7 @@ const Map = () => {
         latitudeDelta: 0.005,
         longitudeDelta: 0.005,
       }}
+      // ref={mapRef}
     >
       {/* {origin && destination && ()} */}
       {/* <MapViewDirections
@@ -60,7 +79,7 @@ const Map = () => {
       <Marker
         coordinate={{
           latitude: 37.78825,
-          longitude: -122.4340,
+          longitude: -122.434,
         }}
         title="Destination"
         description="San Francisco"
--- components/RideOptionsCard.js
+++ components/RideOptionsCard.js
@@ -3,9 +3,13 @@ import React, { useState } from "react";
 import { StyleSheet, Text, SafeAreaView, View } from "react-native";
 import { Icon } from "react-native-elements";
 import { FlatList, TouchableOpacity } from "react-native-gesture-handler";
+import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import tw from "tailwind-react-native-classnames";
-import { setDestination } from "../slices/navSlice";
+import {
+  selectTravelTimeInformation,
+  setDestination,
+} from "../slices/navSlice";
 import NavFavourites from "./NavFavourites";
 
 const data = [
@@ -29,9 +33,13 @@ const data = [
   },
 ];
 
+const SURGE_CHARGE_RATE = 1.5;
+
 const RideOptionsCard = () => {
   const navigation = useNavigation();
   const [selected, setSelected] = useState(null);
+
+  const travelTimeInformation = useSelector(selectTravelTimeInformation);
 
   return (
     <SafeAreaView style={tw`bg-white flex-grow`}>
@@ -47,7 +55,9 @@ const RideOptionsCard = () => {
             size={16}
           />
         </TouchableOpacity>
-        <Text style={tw`text-center py-5 text-xl`}>Select a Ride</Text>
+        <Text style={tw`text-center py-5 text-xl`}>
+          Select a Ride - {travelTimeInformation?.distance.text}
+        </Text>
       </View>
 
       <FlatList
@@ -70,14 +80,28 @@ const RideOptionsCard = () => {
             />
             <View style={tw`-ml-6`}>
               <Text style={tw`text-xl font-semibold`}>{title}</Text>
-              <Text>Travel time...</Text>
+              <Text>
+                Travel time...
+                {travelTimeInformation?.duration.text}
+              </Text>
             </View>
-            <Text style={tw`text-xl`}>$99</Text>
+            <Text style={tw`text-xl`}>
+              $99
+              {new Intl.NumberFormat("en-us", {
+                style: "currency",
+                currency: "USD",
+              }).format(
+                (travelTimeInformation?.duration.value *
+                  SURGE_CHARGE_RATE *
+                  multiplier) /
+                  100
+              )}
+            </Text>
           </TouchableOpacity>
         )}
       />
 
-      <View>
+      <View style={tw`mt-auto border-t border-gray-200`}>
         <TouchableOpacity
           disabled={!selected}
           style={tw`bg-black py-3 m-3 ${!selected && "bg-gray-300"}`}
